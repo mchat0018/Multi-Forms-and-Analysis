a83eeb75e9ddd055903b9036736d522fae2139be
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-07-30 18:28:30 +0530

    Validation complete...to be debugged

--- MSForms/questionnaire/static/questionnaire/jscript.js
+++ MSForms/questionnaire/static/questionnaire/jscript.js
@@ -1,43 +1,108 @@
+$(document).ready(function(){   
+    
     var current_fs, next_fs, previous_fs; //fieldsets
     var left, opacity, scale; //fieldset properties which we will animate
     var animating; //flag to prevent quick multi-click glitches
 
     $(".next").click(function(){
-        if(animating) return false;
-        animating = true;
 
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
-
-        //activate next step on progressbar using the index of next_fs
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function(now, mx) {
-                //as the opacity of current_fs reduces to 0 - stored in "now"
-                //1. scale current_fs down to 80%
-                scale = 1 - (1 - now) * 0.2;
-                //2. bring next_fs from the right(50%)
-                left = (now * 50)+"%";
-                //3. increase opacity of next_fs to 1 as it moves in
-                opacity = 1 - now;
-                current_fs.css({
-                    'transform': 'scale('+scale+')',
-                    'position': 'absolute'
-                });
-                next_fs.css({'left': left, 'opacity': opacity});
+        //validate the current fieldset
+        var form=$("#msform");
+        
+        form.validate({
+            rules:{
+                texture : {
+                    required:true,
+                },
+                state : {
+                    required:true,
+                },
+                length : {
+                    required:true,
+                },
+                thickness : {
+                    required:true,
+                },
+                volume : {
+                    required:true,
+                },
+                scalp : {
+                    required:true,
+                },
+                dandruff : {
+                    required:true,
+                },
+                hair_loss : {
+                    required:true,
+                }
             },
-            duration: 800,
-            complete: function(){
-                current_fs.hide();
-                animating = false;
+
+            messages: {
+                texture : {
+                    required:"Please select one of the options",
+                },
+                state : {
+                    required:"This field is mandatory",
+                },
+                length : {
+                    required:"This field is mandatory",
+                },
+                thickness :  {
+                    required:"This field is mandatory",
+                },
+                volume : {
+                    required:"This field is mandatory",
+                },
+                scalp : {
+                    required:"This field is mandatory",
+                },
+                dandruff : {
+                    required:"This field is mandatory",
+                },
+                hair_loss : {
+                    required:"This field is mandatory",
+                },
             },
-            //this comes from the custom easing plugin
-            easing: 'easeInOutBack'
         });
+
+        if(form.valid()===true){
+            
+            if(animating) return false;
+            animating = true;
+
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+            //activate next step on progressbar using the index of next_fs
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+                step: function(now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale current_fs down to 80%
+                    scale = 1 - (1 - now) * 0.2;
+                    //2. bring next_fs from the right(50%)
+                    left = (now * 50)+"%";
+                    //3. increase opacity of next_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    current_fs.css({
+                        'transform': 'scale('+scale+')',
+                        'position': 'absolute'
+                    });
+                    next_fs.css({'left': left, 'opacity': opacity});
+                },
+                duration: 800,
+                complete: function(){
+                    current_fs.hide();
+                    animating = false;
+                },
+                //this comes from the custom easing plugin
+                easing: 'easeInOutBack'
+            });
+        }
     });
 
     $(".previous").click(function(){
@@ -74,3 +139,4 @@
             easing: 'easeInOutBack'
         });
     });
+});
